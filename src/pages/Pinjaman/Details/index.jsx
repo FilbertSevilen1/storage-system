2a71904831f805449e0bd1c70b780ed42459fd3d
Ignatius Filbert Sevilen <sevilenfilbert@gmail.com>
2024-05-14 15:25:52 +0700

    Update Pinjaman Final

--- src/pages/Pinjaman/Details/index.jsx
+++ src/pages/Pinjaman/Details/index.jsx
@@ -40,9 +40,8 @@ function DetailPinjaman() {
   const [endDate, setEndDate] = useState("01/12/2024");
   const [reason, setReason] = useState("Testing");
   const [statusId, setStatusId] = useState("");
-  const [statusName, setStatusName] = useState(
-    "Menunggu Approval Pengembalian"
-  );
+  const [statusName, setStatusName] = useState();
+  const [approval, setApproval] = useState();
 
   const formatDate = (date) => {
     const dateformat = new Date(date);
@@ -111,6 +110,7 @@ function DetailPinjaman() {
         setEndDate(res.data.borrow.endDate);
         setReason(res.data.borrow.reason);
         setStatusName(res.data.borrow.statusName);
+        setApproval(res.data.borrow.approval);
 
         getDetailPeralatan(res.data.borrow.peralatans);
       })
@@ -515,7 +515,7 @@ function DetailPinjaman() {
                 <b>Tanggal Selesai :</b> {formatDate(endDate)}
               </div>
             </div>
-            <div className="w-full flex flex-wrap">
+            <div className="w-full flex flex-wrap border-b-[0.5px] border-zinc-500">
               <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -552,113 +552,119 @@ function DetailPinjaman() {
               </div>
             </div>
 
-            <div className="w-full flex flex-wrap">
-              <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 16q.425 0 .738-.312t.312-.738t-.312-.737T12 13.9t-.737.313t-.313.737t.313.738T12 16m-.75-3.1h1.5q0-.475.038-.75t.112-.45q.1-.2.288-.412t.612-.638q.525-.525.788-1.05t.262-1.05q0-1.175-.775-1.863T12 6q-1.025 0-1.8.575T9.15 8.1l1.35.55q.175-.575.575-.888T12 7.45q.6 0 .975.325t.375.825q0 .425-.188.738t-.662.712q-.425.35-.675.638t-.375.562q-.125.25-.162.613T11.25 12.9m.75 6.45q3.05-2.8 4.525-5.087T18 10.2q0-2.725-1.737-4.462T12 4T7.738 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35M12 22q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2t5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22m0-12"
-                  />
-                </svg>
-                <div className="ml-2">
-                  <b>Status Persetujuan Mulai</b>
-                  <div>{reason}</div>
+            {approval ? (
+              <>
+                <div className="w-full flex flex-wrap border-b-[0.5px] border-zinc-500">
+                  <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 16q.425 0 .738-.312t.312-.738t-.312-.737T12 13.9t-.737.313t-.313.737t.313.738T12 16m-.75-3.1h1.5q0-.475.038-.75t.112-.45q.1-.2.288-.412t.612-.638q.525-.525.788-1.05t.262-1.05q0-1.175-.775-1.863T12 6q-1.025 0-1.8.575T9.15 8.1l1.35.55q.175-.575.575-.888T12 7.45q.6 0 .975.325t.375.825q0 .425-.188.738t-.662.712q-.425.35-.675.638t-.375.562q-.125.25-.162.613T11.25 12.9m.75 6.45q3.05-2.8 4.525-5.087T18 10.2q0-2.725-1.737-4.462T12 4T7.738 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35M12 22q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2t5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22m0-12"
+                      />
+                    </svg>
+                    <div className="ml-2">
+                      <b>Status Persetujuan Mulai</b>
+                      <div>{approval.approvalStartStatusName?approval.approvalStartStatusName:"Tidak ada Data"}</div>
+                    </div>
+                  </div>
+                  <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M7 12.5h7q.213 0 .357-.143T14.5 12t-.143-.357Q14.213 11.5 14 11.5H7q-.213 0-.357.143T6.5 12t.143.357q.144.143.357.143m0-3h7q.213 0 .357-.143T14.5 9t-.143-.357Q14.213 8.5 14 8.5H7q-.213 0-.357.143T6.5 9t.143.357Q6.787 9.5 7 9.5M4.615 19q-.69 0-1.152-.462T3 17.385V6.615q0-.69.463-1.152T4.615 5h14.77q.69 0 1.152.463T21 6.615v10.77q0 .69-.462 1.152T19.385 19zm0-1h14.77q.23 0 .423-.192t.192-.423V6.615q0-.23-.192-.423T19.385 6H4.615q-.23 0-.423.192T4 6.615v10.77q0 .23.192.423t.423.192M4 18V6z"
+                      />
+                    </svg>
+                    <div className="ml-2">
+                      <b>Alasan Persetujuan Mulai</b>
+                      <div>{approval.approvalStartReason?approval.approvalStartReason:"Tidak ada Data"}</div>
+                    </div>
+                  </div>
+                  <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M5 8h14V6H5zm0 0V6zm0 14q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V3q0-.425.288-.712T7 2t.713.288T8 3v1h8V3q0-.425.288-.712T17 2t.713.288T18 3v1h1q.825 0 1.413.588T21 6v4.675q0 .425-.288.713t-.712.287t-.712-.288t-.288-.712V10H5v10h5.8q.425 0 .713.288T11.8 21t-.288.713T10.8 22zm13 1q-2.075 0-3.537-1.463T13 18t1.463-3.537T18 13t3.538 1.463T23 18t-1.463 3.538T18 23m.5-5.2v-2.3q0-.2-.15-.35T18 15t-.35.15t-.15.35v2.275q0 .2.075.388t.225.337l1.525 1.525q.15.15.35.15t.35-.15t.15-.35t-.15-.35z"
+                      />
+                    </svg>
+                    <div className="ml-2">
+                      <b>Waktu Persetujuan Mulai</b>
+                      <div>{approval.approvalStartDate?formatDate(approval.approvalStartDate):"Tidak ada Data"}</div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M7 12.5h7q.213 0 .357-.143T14.5 12t-.143-.357Q14.213 11.5 14 11.5H7q-.213 0-.357.143T6.5 12t.143.357q.144.143.357.143m0-3h7q.213 0 .357-.143T14.5 9t-.143-.357Q14.213 8.5 14 8.5H7q-.213 0-.357.143T6.5 9t.143.357Q6.787 9.5 7 9.5M4.615 19q-.69 0-1.152-.462T3 17.385V6.615q0-.69.463-1.152T4.615 5h14.77q.69 0 1.152.463T21 6.615v10.77q0 .69-.462 1.152T19.385 19zm0-1h14.77q.23 0 .423-.192t.192-.423V6.615q0-.23-.192-.423T19.385 6H4.615q-.23 0-.423.192T4 6.615v10.77q0 .23.192.423t.423.192M4 18V6z"
-                  />
-                </svg>
-                <div className="ml-2">
-                  <b>Alasan Persetujuan Mulai</b>
-                  <div>{statusName}</div>
-                </div>
-              </div>
-              <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M5 8h14V6H5zm0 0V6zm0 14q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V3q0-.425.288-.712T7 2t.713.288T8 3v1h8V3q0-.425.288-.712T17 2t.713.288T18 3v1h1q.825 0 1.413.588T21 6v4.675q0 .425-.288.713t-.712.287t-.712-.288t-.288-.712V10H5v10h5.8q.425 0 .713.288T11.8 21t-.288.713T10.8 22zm13 1q-2.075 0-3.537-1.463T13 18t1.463-3.537T18 13t3.538 1.463T23 18t-1.463 3.538T18 23m.5-5.2v-2.3q0-.2-.15-.35T18 15t-.35.15t-.15.35v2.275q0 .2.075.388t.225.337l1.525 1.525q.15.15.35.15t.35-.15t.15-.35t-.15-.35z"
-                  />
-                </svg>
-                <div className="ml-2">
-                  <b>Waktu Persetujuan Mulai</b>
-                  <div>{statusName}</div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full flex flex-wrap">
-              <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 15.365q.271 0 .468-.197t.197-.468t-.197-.468t-.468-.197t-.468.197t-.197.468t.197.468t.468.197m2.542-6.738q0-1.021-.688-1.66q-.689-.64-1.767-.64q-.977 0-1.66.477t-.7 1.017q0 .154.061.25q.062.094.204.13q.16.043.297-.04t.234-.205q.187-.352.573-.554q.387-.202.967-.202q.69 0 1.143.409t.452 1.018q0 .546-.29.888q-.29.343-.693.71q-.529.487-.817.96q-.289.473-.289.961q0 .208.12.337t.317.129q.19 0 .313-.124q.123-.123.135-.325q.011-.355.309-.727t.583-.66q.479-.478.838-.987t.358-1.162M12 20.556q-.235 0-.47-.077t-.432-.25q-1.067-.98-2.163-2.185q-1.097-1.204-1.992-2.493t-1.467-2.633t-.572-2.622q0-3.173 2.066-5.234T12 3t5.03 2.062t2.066 5.234q0 1.279-.572 2.613q-.572 1.333-1.458 2.632q-.885 1.3-1.981 2.494T12.92 20.21q-.198.173-.442.26t-.479.086"
-                  />
-                </svg>
-                <div className="ml-2">
-                  <b>Status Persetujuan Selesai</b>
-                  <div>{reason}</div>
-                </div>
-              </div>
-              <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M8.117 16.538q.327 0 .547-.221t.22-.55t-.22-.547t-.55-.22t-.548.222t-.22.549t.222.547q.221.22.549.22m0-3.769q.327 0 .547-.221t.22-.55t-.22-.547t-.55-.22t-.548.221t-.22.55t.222.547q.221.22.549.22m0-3.769q.327 0 .547-.222t.22-.549t-.22-.547t-.55-.22t-.548.221t-.22.55t.222.547q.221.22.549.22m3.575 7.27h4.385q.212 0 .356-.145q.144-.144.144-.356q0-.213-.144-.357t-.356-.143h-4.385q-.212 0-.356.144t-.144.357t.144.356t.356.143m0-3.769h4.385q.212 0 .356-.144t.144-.357t-.144-.356t-.356-.143h-4.385q-.212 0-.356.144t-.144.357t.144.356t.356.143m0-3.77h4.385q.212 0 .356-.143t.144-.357t-.144-.356t-.356-.143h-4.385q-.212 0-.356.144t-.144.356t.144.357t.356.143M5.615 20q-.69 0-1.152-.462T4 18.385V5.615q0-.69.463-1.152T5.615 4h12.77q.69 0 1.152.463T20 5.615v12.77q0 .69-.462 1.152T18.385 20z"
-                  />
-                </svg>
-                <div className="ml-2">
-                  <b>Alasan Persetujuan Selesai</b>
-                  <div>{statusName}</div>
-                </div>
-              </div>
+                <div className="w-full flex flex-wrap">
+                  <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 15.365q.271 0 .468-.197t.197-.468t-.197-.468t-.468-.197t-.468.197t-.197.468t.197.468t.468.197m2.542-6.738q0-1.021-.688-1.66q-.689-.64-1.767-.64q-.977 0-1.66.477t-.7 1.017q0 .154.061.25q.062.094.204.13q.16.043.297-.04t.234-.205q.187-.352.573-.554q.387-.202.967-.202q.69 0 1.143.409t.452 1.018q0 .546-.29.888q-.29.343-.693.71q-.529.487-.817.96q-.289.473-.289.961q0 .208.12.337t.317.129q.19 0 .313-.124q.123-.123.135-.325q.011-.355.309-.727t.583-.66q.479-.478.838-.987t.358-1.162M12 20.556q-.235 0-.47-.077t-.432-.25q-1.067-.98-2.163-2.185q-1.097-1.204-1.992-2.493t-1.467-2.633t-.572-2.622q0-3.173 2.066-5.234T12 3t5.03 2.062t2.066 5.234q0 1.279-.572 2.613q-.572 1.333-1.458 2.632q-.885 1.3-1.981 2.494T12.92 20.21q-.198.173-.442.26t-.479.086"
+                      />
+                    </svg>
+                    <div className="ml-2">
+                      <b>Status Persetujuan Selesai</b>
+                      <div>{approval.approvalEndStatusName?approval.approvalEndStatusName:"Tidak ada Data"}</div>
+                    </div>
+                  </div>
+                  <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M8.117 16.538q.327 0 .547-.221t.22-.55t-.22-.547t-.55-.22t-.548.222t-.22.549t.222.547q.221.22.549.22m0-3.769q.327 0 .547-.221t.22-.55t-.22-.547t-.55-.22t-.548.221t-.22.55t.222.547q.221.22.549.22m0-3.769q.327 0 .547-.222t.22-.549t-.22-.547t-.55-.22t-.548.221t-.22.55t.222.547q.221.22.549.22m3.575 7.27h4.385q.212 0 .356-.145q.144-.144.144-.356q0-.213-.144-.357t-.356-.143h-4.385q-.212 0-.356.144t-.144.357t.144.356t.356.143m0-3.769h4.385q.212 0 .356-.144t.144-.357t-.144-.356t-.356-.143h-4.385q-.212 0-.356.144t-.144.357t.144.356t.356.143m0-3.77h4.385q.212 0 .356-.143t.144-.357t-.144-.356t-.356-.143h-4.385q-.212 0-.356.144t-.144.356t.144.357t.356.143M5.615 20q-.69 0-1.152-.462T4 18.385V5.615q0-.69.463-1.152T5.615 4h12.77q.69 0 1.152.463T20 5.615v12.77q0 .69-.462 1.152T18.385 20z"
+                      />
+                    </svg>
+                    <div className="ml-2">
+                      <b>Alasan Persetujuan Selesai</b>
+                      <div>{approval.approvalEndReason?approval.approvalEndReason:"Tidak ada Data"}</div>
+                    </div>
+                  </div>
 
-              <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V3q0-.425.288-.712T7 2t.713.288T8 3v1h8V3q0-.425.288-.712T17 2t.713.288T18 3v1h1q.825 0 1.413.588T21 6v4.675q0 .425-.288.713t-.712.287t-.712-.288t-.288-.712V10H5v10h5.8q.425 0 .713.288T11.8 21t-.288.713T10.8 22zm13 1q-2.075 0-3.537-1.463T13 18t1.463-3.537T18 13t3.538 1.463T23 18t-1.463 3.538T18 23m.5-5.2v-2.3q0-.2-.15-.35T18 15t-.35.15t-.15.35v2.275q0 .2.075.388t.225.337l1.525 1.525q.15.15.35.15t.35-.15t.15-.35t-.15-.35z"
-                  />
-                </svg>
-                <div className="ml-2">
-                  <b>Waktu Persetujuan Selesai</b>
-                  <div>{statusName}</div>
+                  <div className="flex w-full md:w-1/2 xl:w-1/4 items-center h-16">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M5 22q-.825 0-1.412-.587T3 20V6q0-.825.588-1.412T5 4h1V3q0-.425.288-.712T7 2t.713.288T8 3v1h8V3q0-.425.288-.712T17 2t.713.288T18 3v1h1q.825 0 1.413.588T21 6v4.675q0 .425-.288.713t-.712.287t-.712-.288t-.288-.712V10H5v10h5.8q.425 0 .713.288T11.8 21t-.288.713T10.8 22zm13 1q-2.075 0-3.537-1.463T13 18t1.463-3.537T18 13t3.538 1.463T23 18t-1.463 3.538T18 23m.5-5.2v-2.3q0-.2-.15-.35T18 15t-.35.15t-.15.35v2.275q0 .2.075.388t.225.337l1.525 1.525q.15.15.35.15t.35-.15t.15-.35t-.15-.35z"
+                      />
+                    </svg>
+                    <div className="ml-2">
+                      <b>Waktu Persetujuan Selesai</b>
+                      <div>{approval.approvalEndDate?formatDate(approval.approvalEndDate):"Tidak ada Data"}</div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              </>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
         <div className="bg-white w-full flex flex-col items-center mt-8 shadow-md px-4 py-4 mb-8">
@@ -755,7 +761,7 @@ function DetailPinjaman() {
                     variant="contained"
                     size="large"
                   >
-                    Selesaikan Pinjaman
+                    Penyelesaian Pinjaman
                   </Button>
                 </div>
               </div>
